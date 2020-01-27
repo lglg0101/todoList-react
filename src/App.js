311eb335eeab4e29b9703673645717c978e37b4b
your username <lissygundersen@gmail.com>
2020-01-27 19:45:27 +0000

    todolist component nearly completed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,45 @@
-import React, { Component } from 'react'
-import './index.css'
+import React, { Component } from "react";
+import "./index.css";
 
-import TodoList from './components/TodoList'
-
-
+import TodoList from "./components/TodoList";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      items: [],
+      currentItem: {
+        text: "",
+        key: ""
+      }
+    };
+  }
+
+  //manages the change in the inoput field
+  handleInput = e => {
+    console.log("input");
+  };
+
+  //manages to add to the list
+  addItem = e => {
+    e.preventDefault();
+    console.log("add item");
+  };
+
   render() {
     return (
       <div className="App">
-            <div>
-                <h1>Testing</h1>
-                <TodoList />
-            </div>
+        <div>
+          <h1>Testing</h1>
+          <TodoList 
+          addItem={this.addItem} 
+          inputElement={this.inputElement}
+          handleInput={this.handleInput}
+          currentItem={this.state.currentItem}
+          />
+        </div>
       </div>
-    )
+    );
   }
 }
-export default App
+export default App;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react'
+
 class TodoList extends Component {
+componentDidUpdate() {
+    this.props.inputElement.current.focus()
+}
+
+    //create a reference to refer to the input 
+  inputElement = React.createRef()
+
   render() {
     return (
       <div className="todoListMain">
         <div className="header">
-          <form>
-            <input placeholder="Task" />
+
+       {/* form on submit, calls add item  */}
+          <form onSubmit={this.props.addItem}>
+              
+            <input 
+            //refers to current element
+            ref = {this.props.inputElement} 
+            //stores as tet in the current element object
+            value={this.props.currentItem.text} 
+            //neccessary to call handleInput
+            onChange={this.props.handleInput} 
+            placeholder="Task" />
+
             <button type="submit"> Add Task </button>
           </form>
         </div>
