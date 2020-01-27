7e4da609fff7599ea20da1e7f2edfcedc880273d
your username <lissygundersen@gmail.com>
2020-01-27 19:10:03 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './index.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import TodoList from './components/TodoList'
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+            <div>
+                <h1>Testing</h1>
+                <TodoList />
+            </div>
+      </div>
+    )
+  }
 }
-
-export default App;
+export default App
